3324a2eaccb8f3ea3ae2a82ba68adab9aaebed17
mononokeGit <mononoke_0@outlook.com>
2022-02-28 16:34:43 +0800

    0.01

---
+++ src/components/jdb_lis.vue
@@ -0,0 +1,17 @@
+<template>
+      <div>
+    <input type="checkbox" id="jdb_lis" checked="false" />
+    <label for="jdb_lis">接待方案</label>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<!--/src/views/Login.vue-->
+<template>
+  <div class="login-body">
+    登录
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+  .login-body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    background-color: #fff;
+  }
+</style>
